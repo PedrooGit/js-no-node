2bca5ea090480b6e0b2ca0d8cbc674f6e5488363
PedrooGit <pedroogit@gmail.com>
2026-05-10 18:51:14 -0300

    extrair obj

--- index.js
+++ index.js
@@ -15,16 +15,50 @@
 // console.log(pc);
 
 
+// if (true){
+//     var a = 1;  
+//     let b = 2;
+//     const c = 3;
+// }
+// console.log(a);
 
 
 
-if (true){
-    var a = 1;  
-    let b = 2;
-    const c = 3;
+function main(){
+
+    const pessoa = {
+
+        name: ' ',
+        idade: 18,
+        vivo: true
+    }
+
+    // const name = pessoa.nome;
+    // msm coisa
+
+    // extrair coisas de objs
+    const {name} = pessoa;
+    console.log(name);
 }
-console.log(a);
+main();
 
+function kct_queLegal(){
 
+    const foda = {
 
+        queLegal: true,
+        legalMsm: "SIM",
+        escola: {
+            ano_1: 'passeii',
+            ano_2: "passei tamb",
+            ano_3: "ainda nao"
+        }
+    }
 
+    const {escola} = foda;
+    const {ano_1} = escola;
+
+    console.log(ano_1);
+
+}
+kct_queLegal();
