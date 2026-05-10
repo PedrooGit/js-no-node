4f83fafe0b100a1960cc6474ca69dec565c22141
PedrooGit <pedroogit@gmail.com>
2026-05-10 18:21:27 -0300

    java no node, obj

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const pc = {
+
+    placaMae: "B341",
+    placaVideo: 'rtx1341',
+    cpu: "intel ckt",
+    fonte:  {
+        name: 'caixa de sapato',
+        volts: 12,
+        wats: 600
+    },
+    ram: [1,2,3,4],
+}
+console.log(pc);
+
+
+
+
+
+
+
+
+
+
+
