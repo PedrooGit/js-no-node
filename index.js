223d95df433f5ad7a9b233499e9bfc95136155c7
PedrooGit <pedroogit@gmail.com>
2026-05-10 18:37:45 -0300

    var, let, const

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 
 
-const pc = {
+// const pc = {
 
-    placaMae: "B341",
-    placaVideo: 'rtx1341',
-    cpu: "intel ckt",
-    fonte:  {
-        name: 'caixa de sapato',
-        volts: 12,
-        wats: 600
-    },
-    ram: [1,2,3,4],
+//     placaMae: "B341",
+//     placaVideo: 'rtx1341',
+//     cpu: "intel ckt",
+//     fonte:  {
+//         name: 'caixa de sapato',
+//         volts: 12,
+//         wats: 600
+//     },
+//     ram: [1,2,3,4],
+// }
+// console.log(pc);
+
+
+
+
+
+if (true){
+    var a = 1;  
+    let b = 2;
+    const c = 3;
 }
-console.log(pc);
-
-
-
-
-
-
-
+console.log(a);
 
 
 
